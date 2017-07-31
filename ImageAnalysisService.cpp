cea97ffecbb801cf074cd021f65cfdd16fe98b39
Douglas Teeple <doug@douglasteeple.com>
2017-07-31 13:58:49 -1000

    Documentation update

--- ImageAnalysisService.cpp
+++ ImageAnalysisService.cpp
@@ -9,6 +9,17 @@
 #include <opencv2/opencv.hpp>
 #include "ImageAnalysisService.hpp"
 
+// TODO: Implement exceptions
+
+/*
+ *  FIND_REGION accepts an RGB image, an (X, Y) pixel location, and a distance parameter to
+ *  control a greedy style algorithm that "flood fills" the nearby pixels in order to find
+ *  a contiguous patch of pixels that are similar in color. The return value is a vector of
+ *  cv::Points. The RGB image is converted to HSV to easily select by color (H). The maximum
+ *  distance from the color at point x,y is given as a parameter. The hue at the x,y point
+ *  is optionally returned in the "*hue" argument. Returns empty vector if x or y outside
+ *  image range.
+ */
 std::vector<cv::Point> ImageAnalysisService::FIND_REGION(const cv::Mat &image, int x, int y, int distance, int *hue) {
     std::vector<cv::Point> pointvector;
     if (image.empty()) {                      // Check for invalid input
@@ -40,6 +51,11 @@ std::vector<cv::Point> ImageAnalysisService::FIND_REGION(const cv::Mat &image, i
     }
     return pointvector;
 }
+
+/*
+ *  FIND_PERIMETER accepts the output of FIND_REGION and outputs contiguous pixels that
+ *  border the region. These pixels should be a subset of pixels returned by FIND_REGION.
+ */
 std::vector<cv::Point> ImageAnalysisService::FIND_PERIMETER(const std::vector<cv::Point> &region) {
     std::vector<cv::Point> pointvector;
     int maxrows = 0;
@@ -81,12 +97,21 @@ std::vector<cv::Point> ImageAnalysisService::FIND_PERIMETER(const std::vector<cv
     }
     return pointvector;
 }
+
+/*
+ *  DISPLAY_IMAGE provides a method for displaying loaded RGB images.
+ */
 void ImageAnalysisService::DISPLAY_IMAGE(const cv::Mat &image, std::string const &win_name) {
     cv::namedWindow(win_name, cv::WINDOW_AUTOSIZE);
     cvtColor(image, image, cv::COLOR_RGB2BGR);  // opencv displays BGR, we use RGB internally
     cv::imshow(win_name, image);
     cv::waitKey(1000);
 }
+
+/*
+ *  DISPLAY_PIXELS provides a method for displaying an image representation of the
+ *  results from the above FIND_* operators.
+ */
 void ImageAnalysisService::DISPLAY_PIXELS(const std::vector<cv::Point> &region, std::string const &win_name) {
     int maxrows = 0;
     int maxcols = 0;
@@ -113,6 +138,9 @@ void ImageAnalysisService::DISPLAY_PIXELS(const std::vector<cv::Point> &region, 
     }
 }
 
+/*
+ *  SAVE_PIXELS accepts the vector output from FIND_* and saves the image to a file.
+ */
 void ImageAnalysisService::SAVE_PIXELS(const std::vector<cv::Point> &region, std::string const &file_name) {
     int maxrows = 0;
     int maxcols = 0;
@@ -136,6 +164,9 @@ void ImageAnalysisService::SAVE_PIXELS(const std::vector<cv::Point> &region, std
     }
 }
 
+/*
+ *  SAVE_IMAGE saves the image to a file.
+ */
 void ImageAnalysisService::SAVE_IMAGE(const cv::Mat &image, std::string const &file_name) {
     imwrite(file_name, image);
 }
