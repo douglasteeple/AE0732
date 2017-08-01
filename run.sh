62db50f8ff8798dabe9ba42e9bf10ff101c3b00c
Douglas Teeple <doug@douglasteeple.com>
2017-08-01 11:04:23 -1000

    Change to accept extra command argument

--- run.sh
+++ run.sh
@@ -28,45 +28,45 @@
 #* (2200,600)
 #* (3300,1420) 
 #
-echo ./ImageAnalysisService AeolusTestImages/test1.png 58 147 $1
-./ImageAnalysisService AeolusTestImages/test1.png 58 147 $1
-echo ./ImageAnalysisService AeolusTestImages/test1.png 19 33 $1
-./ImageAnalysisService AeolusTestImages/test1.png 19 33 $1
-echo ./ImageAnalysisService AeolusTestImages/test1.png 58 100 $1
-./ImageAnalysisService AeolusTestImages/test1.png 58 100 $1
-echo ./ImageAnalysisService AeolusTestImages/test1.png 100 100 $1
-./ImageAnalysisService AeolusTestImages/test1.png 100 100 $1
-echo ./ImageAnalysisService AeolusTestImages/test1.png 122 60 $1
-./ImageAnalysisService AeolusTestImages/test1.png 122 60 $1
+echo ./ImageAnalysisService AeolusTestImages/test1.png 58 147 $*
+./ImageAnalysisService AeolusTestImages/test1.png 58 147 $*
+echo ./ImageAnalysisService AeolusTestImages/test1.png 19 33 $*
+./ImageAnalysisService AeolusTestImages/test1.png 19 33 $*
+echo ./ImageAnalysisService AeolusTestImages/test1.png 58 100 $*
+./ImageAnalysisService AeolusTestImages/test1.png 58 100 $*
+echo ./ImageAnalysisService AeolusTestImages/test1.png 100 100 $*
+./ImageAnalysisService AeolusTestImages/test1.png 100 100 $*
+echo ./ImageAnalysisService AeolusTestImages/test1.png 122 60 $*
+./ImageAnalysisService AeolusTestImages/test1.png 122 60 $*
 
-echo ./ImageAnalysisService AeolusTestImages/test2.png 90 140 $1
-./ImageAnalysisService AeolusTestImages/test2.png 90 140 $1
-echo ./ImageAnalysisService AeolusTestImages/test2.png 58 100 $1
-./ImageAnalysisService AeolusTestImages/test2.png 58 100 $1
-echo ./ImageAnalysisService AeolusTestImages/test2.png 100 100 $1
-./ImageAnalysisService AeolusTestImages/test2.png 100 100 $1
-echo ./ImageAnalysisService AeolusTestImages/test2.png 164 72 $1
-./ImageAnalysisService AeolusTestImages/test2.png 164 72 $1
-echo ./ImageAnalysisService AeolusTestImages/test2.png 122 98 $1
-./ImageAnalysisService AeolusTestImages/test2.png 122 98 $1
+echo ./ImageAnalysisService AeolusTestImages/test2.png 90 140 $*
+./ImageAnalysisService AeolusTestImages/test2.png 90 140 $*
+echo ./ImageAnalysisService AeolusTestImages/test2.png 58 100 $*
+./ImageAnalysisService AeolusTestImages/test2.png 58 100 $*
+echo ./ImageAnalysisService AeolusTestImages/test2.png 100 100 $*
+./ImageAnalysisService AeolusTestImages/test2.png 100 100 $*
+echo ./ImageAnalysisService AeolusTestImages/test2.png 164 72 $*
+./ImageAnalysisService AeolusTestImages/test2.png 164 72 $*
+echo ./ImageAnalysisService AeolusTestImages/test2.png 122 98 $*
+./ImageAnalysisService AeolusTestImages/test2.png 122 98 $*
 
-echo ./ImageAnalysisService AeolusTestImages/test3.png 58 100 $1
-./ImageAnalysisService AeolusTestImages/test3.png 58 100 $1
-echo ./ImageAnalysisService AeolusTestImages/test3.png 100 100 $1
-./ImageAnalysisService AeolusTestImages/test3.png 100 100 $1
-echo ./ImageAnalysisService AeolusTestImages/test3.png 156 103 $1
-./ImageAnalysisService AeolusTestImages/test3.png 156 103 $1
+echo ./ImageAnalysisService AeolusTestImages/test3.png 58 100 $*
+./ImageAnalysisService AeolusTestImages/test3.png 58 100 $*
+echo ./ImageAnalysisService AeolusTestImages/test3.png 100 100 $*
+./ImageAnalysisService AeolusTestImages/test3.png 100 100 $*
+echo ./ImageAnalysisService AeolusTestImages/test3.png 156 103 $*
+./ImageAnalysisService AeolusTestImages/test3.png 156 103 $*
 
-echo ./ImageAnalysisService AeolusTestImages/test4.jpg 416 486 $1
-./ImageAnalysisService AeolusTestImages/test4.jpg 416 486 $1
-echo ./ImageAnalysisService AeolusTestImages/test4.jpg 1510 640 $1
-./ImageAnalysisService AeolusTestImages/test4.jpg 1510 640 $1
-echo ./ImageAnalysisService AeolusTestImages/test4.jpg 1590 1940 $1
-./ImageAnalysisService AeolusTestImages/test4.jpg 1590 1940 $1
-echo ./ImageAnalysisService AeolusTestImages/test4.jpg 2200 600 $1
-./ImageAnalysisService AeolusTestImages/test4.jpg 2200 600 $1
-echo ./ImageAnalysisService AeolusTestImages/test4.jpg 3300 1420 $1
-./ImageAnalysisService AeolusTestImages/test4.jpg 3300 1420 $1
+echo ./ImageAnalysisService AeolusTestImages/test4.jpg 416 486 $*
+./ImageAnalysisService AeolusTestImages/test4.jpg 416 486 $*
+echo ./ImageAnalysisService AeolusTestImages/test4.jpg 1510 640 $*
+./ImageAnalysisService AeolusTestImages/test4.jpg 1510 640 $*
+echo ./ImageAnalysisService AeolusTestImages/test4.jpg 1590 1940 $*
+./ImageAnalysisService AeolusTestImages/test4.jpg 1590 1940 $*
+echo ./ImageAnalysisService AeolusTestImages/test4.jpg 2200 600 $*
+./ImageAnalysisService AeolusTestImages/test4.jpg 2200 600 $*
+echo ./ImageAnalysisService AeolusTestImages/test4.jpg 3300 1420 $*
+./ImageAnalysisService AeolusTestImages/test4.jpg 3300 1420 $*
 
 
 
